684e4398241dda1e268a2d2c1b3f0a0780e254ea
Zzzyii <147442775+Zzzyii@users.noreply.github.com>
2024-05-09 19:12:24 +0800

    Add GRU model

--- GRU_Model/model.py
+++ GRU_Model/model.py
@@ -1,1 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Embedding, GRU, GlobalMaxPooling1D, Dense
 
+# Building a GRU model
+def build_gru_model(vocab_size, embedding_dim,sequence_length):
+    inputs = Input(shape=(sequence_length,), dtype=tf.int32)
+    x = Embedding(vocab_size, embedding_dim)(inputs)
+    x = GRU(16, return_sequences=True,activation='relu')(x)
+    x = GlobalMaxPooling1D()(x)
+    x = Dense(128, activation="relu")(x)
+    outputs = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model

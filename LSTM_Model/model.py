f4144a27a55df7c000b2b948aa69c85a0e6759ed
Zzzyii <147442775+Zzzyii@users.noreply.github.com>
2024-05-09 19:13:52 +0800

    Add LSTM model

--- LSTM_Model/model.py
+++ LSTM_Model/model.py
@@ -1,1 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Embedding, LSTM, GlobalMaxPooling1D, Dense
 
+# Building a LSTM model
+def build_lstm_model(vocab_size, embedding_dim,sequence_length):
+    inputs = Input(shape=(sequence_length,), dtype=tf.int32)
+    x = Embedding(vocab_size, embedding_dim)(inputs)
+    x = LSTM(16, return_sequences=True, activation='relu')(x)
+    x = GlobalMaxPooling1D()(x)
+    x = Dense(128, activation="relu")(x)
+    outputs = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model

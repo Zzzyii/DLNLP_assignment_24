a0bb2e739a9393849cc10b8b510a293ea7a756b4
Zzzyii <147442775+Zzzyii@users.noreply.github.com>
2024-05-09 19:10:13 +0800

    Add CNN model

--- CNN_Model/model.py
+++ CNN_Model/model.py
@@ -1,1 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Embedding, Conv1D, GlobalMaxPooling1D, Dense
 
+# Build a CNN model
+def build_cnn_model(vocab_size, embedding_dim,sequence_length):
+    inputs = Input(shape=(sequence_length,), dtype=tf.int32)
+    x = Embedding(vocab_size, embedding_dim)(inputs)
+    x = Conv1D(64, 3, activation='relu')(x)
+    x = GlobalMaxPooling1D()(x)
+    x = Dense(20, activation="relu")(x)
+    outputs = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
